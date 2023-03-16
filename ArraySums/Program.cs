ec460ab5d2c73767a9d57c84ea7ee3fd9ea89405
Diogo22001907 <diogo.mv.ferreira19@outlook.pt>
2023-03-16 23:43:31 +0000

    Added the user columns, rows and values

---
+++ ArraySums/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ArraySums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] matrix = new int[3][];
+            matrix[0] = new int[4] {0, 1, 2, 3 };
+            matrix[1] = new int[2] {4, 5};
+            matrix[2] = new int[3] {6, 7, 8};
+
+            int valor = matrix[0][3];
+            Console.WriteLine(valor);
+        }
+    }
+}
