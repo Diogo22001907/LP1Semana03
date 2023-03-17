e14ce80f6a6e79da636bcb1ce2cbea1e1f1eb7d0
Diogo22001907 <diogo.mv.ferreira19@outlook.pt>
2023-03-17 14:23:59 +0000

    prints the perk for each character

---
+++ PlayerPerks/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace PlayerPerks
+{
+    class Program
+    {
+        enum PlayerPerks
+        {
+            WaterBreathing = 'w',
+            Stealth = 's',
+            AutoHeal = 'a',
+            DoubleJump = 'd'
+        }
+
+        static void Main(string[] args)
+        {
+            
+            foreach (char c in args[0])
+            {
+                switch (c)
+                {
+                    case 'w':
+                        Console.WriteLine("WaterBreathing");
+                        break;
+                    case 's':
+                
+                        Console.WriteLine("Stealth");
+                        break;
+                    case 'a':
+                        Console.WriteLine("AutoHeal");
+                        break;
+                    case 'd':
+                        Console.WriteLine("DoubleJump");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid perk: " + c);
+                        break;
+                }
+            }
+        }
+    }
+}
